06f816e8d6643a3f6d8dd136d5b424391cf0e8d1
Jordan Mourandee <jordanmourandee@yahoo.fr>
2022-06-20 14:38:42 +0200

    interpolation: communication entre la classe et le template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './moke-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'ng-pokemon-app';
-  pokemonList = ['Bulbizarre', 'Salamèche', 'Caracpuce'];
+  title = 'Liste de Pokémons';
+  pokemonList: Pokemon[] = POKEMONS;
 
   ngOnInit(): void {
     console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
   }
 }
