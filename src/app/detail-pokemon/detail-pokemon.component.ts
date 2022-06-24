f396e70b81e86e203287e7e8b732b75288d976da
Jordan Mourandee <jordanmourandee@yahoo.fr>
2022-06-24 11:43:05 +0200

    conflits

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styleUrls: ['./detail-pokemon.component.scss']
+  styleUrls: ['./detail-pokemon.component.scss'],
 })
 export class DetailPokemonComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
